540dddf41c2418fcc2dfc83318718d0f5280344e
diegotco <22822405+diegotco@users.noreply.github.com>
2024-09-17 16:42:42 -0600

    Create diegocoin.sol

---
+++ diegocoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Diegocoin is ERC20 {
+    uint256 private constant MAX_SUPPLY = 21_000_000 * 10**18; // 21 million tokens with 18 decimals
+
+    constructor() ERC20("Diegocoin", "DIEGO") {
+        _mint(msg.sender, MAX_SUPPLY);
+    }
+
+    function mint(uint256 amount) public pure {
+        revert("Minting is not allowed for this token");
+    }
+}
